82441c28fc7e7f07532460c6fb56a709e665403d
Дарья <kud4v@yandex.ru>
2022-09-30 11:19:22 +0300

    Initial commit

---
+++ core/src/com/example/flappybirdclone/FlappyBird.java
@@ -0,0 +1,50 @@
+package com.example.flappybirdclone;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class FlappyBird extends ApplicationAdapter {
+	SpriteBatch batch;	// класс компьютерной граффики
+	Texture background;	// фон
+	//Texture bird_wings_up;
+	Texture[] bird;
+	int birdStateFlag = 0;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		background = new Texture("background.png");
+		bird = new Texture[2];
+		bird[0] = new Texture("bird_wings_up.png");
+		bird[1] = new Texture("bird_wings_down.png");
+
+	}
+
+	@Override
+	public void render () {
+
+		if(Gdx.input.justTouched()){
+			Gdx.app.log("Tap", "Oops!");
+		}
+		if(birdStateFlag == 0){
+			birdStateFlag = 1;
+		} else birdStateFlag = 0;
+
+		batch.begin();
+		batch.draw(background, 0,0, Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+		batch.draw(bird[birdStateFlag],
+				Gdx.graphics.getWidth() / 2 - bird[birdStateFlag].getWidth() / 2,
+				Gdx.graphics.getHeight() / 2 - bird[birdStateFlag].getHeight() / 2);
+
+		batch.end();
+
+	}
+	
+	@Override
+	public void dispose () {
+
+	}
+}
